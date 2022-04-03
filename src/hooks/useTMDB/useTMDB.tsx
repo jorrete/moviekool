294dd0c691584ed5fe7d0bce8967b46e092f0afa
jorro <jorrete@gmail.com>
2022-04-03 12:13:16 +0200

    Real data calls with TMDB API

--- src/hooks/useTMDB/useTMDB.tsx
+++ src/hooks/useTMDB/useTMDB.tsx
@@ -1,39 +1,41 @@
 import { useMemo } from 'react';
+const TMDB_API_TOKEN = 'd3d74742b1c44b96de48da9da2ea3c8b';
 
 export interface MovieListItemInterface {
   id: number,
-  name: string,
+  original_title: string,
+}
+
+export interface MovieListItemInterface {
+  page: number,
+  results: MovieListItemInterface[],
+  total_pages: number,
+  total_results: number,
 }
 
 export interface MovieDetailInterface {
   id: number,
-  name: string,
+  original_title: string,
 }
 
 interface TMDBInterface {
-  getMovieList: () => MovieListItemInterface[],
-  getMovieDetail: (id: number) => MovieDetailInterface,
+  getMovieList: () => Promise<MovieListItemInterface>,
+  getMovieDetail: (id: number) => Promise<MovieDetailInterface>,
 }
 
-const movies = [
-  {
-    id: 0,
-    name: 'foo',
-  },
-  {
-    id: 1,
-    name: 'bar',
-  },
-  {
-    id: 2,
-    name: 'mongo',
-  },
-];
+function buildAPICall(path: string): string {
+  return `https://api.themoviedb.org/3${path}?api_key=${TMDB_API_TOKEN}`;
+}
+
+function APICall(path: string) {
+  return fetch(buildAPICall(path))
+    .then((response) => response.json());
+}
 
 function useTMDB(): TMDBInterface {
   return useMemo(() => ({
-    getMovieList: () => movies,
-    getMovieDetail: (id) => movies[id],
+    getMovieList: (): Promise<MovieListItemInterface> => APICall('/movie/popular'),
+    getMovieDetail: (id): Promise<MovieDetailInterface>=> APICall(`/movie/${id}`),
   }), []);
 }
 
